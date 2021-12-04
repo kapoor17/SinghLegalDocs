f0d8764afb1deee25d8def3c04120c552b293b6c
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2021-12-04 12:49:22 +0530

    mobile nav done

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const menuBtn=document.querySelector(".menu-btn");
+const navItems=document.querySelector(".nav-links__burger");
+const contactForm = document.querySelector(".contact form");
+
+
 let menuOpen=false;
 menuBtn.addEventListener("click",()=>{
     if(!menuOpen){
         menuBtn.classList.add("open");
         menuOpen=true;
+        navItems.classList.remove("closed")
     }
     else{
         menuBtn.classList.remove("open");
         menuOpen=false;
+        navItems.classList.add("closed");
     }
-
+    
 })
+
+const updateValue=(value)=>{
+    console.log(contactForm.value);
+}
