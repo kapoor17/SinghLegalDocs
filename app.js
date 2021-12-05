78b5df6b018ca710c1b384bb8efe768cad0dac80
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2021-12-05 13:32:14 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,23 +1,64 @@
+//dom objects
+
 const menuBtn=document.querySelector(".menu-btn");
 const navItems=document.querySelector(".nav-links__burger");
-const contactForm = document.querySelector(".contact form");
-
-
 let menuOpen=false;
-menuBtn.addEventListener("click",()=>{
-    if(!menuOpen){
-        menuBtn.classList.add("open");
-        menuOpen=true;
-        navItems.classList.remove("closed")
+
+const contactForm = document.querySelectorAll(".contact form .row input");
+const cards = document.querySelectorAll(".info-cards .cards div");
+
+
+//functions
+
+const checkScreenWidth=()=>{
+    var screenWidth=window.innerWidth;
+    if(screenWidth<568){
+        phoneFunctions();
     }
     else{
-        menuBtn.classList.remove("open");
-        menuOpen=false;
-        navItems.classList.add("closed");
+        notPhoneFunctions();
     }
-    
-})
-
-const updateValue=(value)=>{
-    console.log(contactForm.value);
 }
+
+const burgerMenu=()=>{
+    menuBtn.addEventListener("click",()=>{
+        if(!menuOpen){
+            menuBtn.classList.add("open");
+            menuOpen=true;
+            navItems.classList.remove("closed")
+        }
+        else{
+            menuBtn.classList.remove("open");
+            menuOpen=false;
+            navItems.classList.add("closed"); 
+        }
+    })
+}
+
+
+
+
+const phoneFunctions=()=>{
+    cards.forEach(card=>{
+        card.classList.add("col-11");
+        card.classList.remove("col-6");
+    })
+    
+}
+
+const notPhoneFunctions=()=>{
+    cards.forEach(card=>{
+        card.classList.remove("col-11");
+        card.classList.add("col-6");
+    })
+    
+}
+
+//event listeners
+
+window.onresize=checkScreenWidth;
+
+//function calling
+
+checkScreenWidth();
+burgerMenu();
