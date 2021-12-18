44e2ab24fb086da097a366952453700fa28fce35
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2021-12-18 17:07:12 +0530

    new text material added

--- js/home.js
+++
@@ -1,6 +0,0 @@
-// const showMoreButton = document.querySelector(".show-more");
-// const immigratinoShowMore=document.querySelector("#immigration-show-more")
-
-// showMoreButton.addEventListener("click",()=>{
-//     immigratinoShowMore.classList.toggle("closed")
-// })
